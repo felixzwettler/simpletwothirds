6fe9bc293116c9d7ac6d258a83400a3911d240aa
fpunktz <f.zwettler@posteo.de>
2020-04-02 04:30:16 +0200

    Delete git_clean_commit_hist.sh

--- git_clean_commit_hist.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# git_clean_commit_hist.sh
-
-git checkout --orphan latest_branch
-git add -A
-git commit -am "init"
-git branch -D master
-git branch -m master
-git push -f origin master
